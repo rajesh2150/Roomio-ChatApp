417455771b19b13c7484e9256356c34632317985
Rajesh Korlapati <rajeshkorlapati465@gmail.com>
2024-06-17 21:31:13 +0530

    REFACTOR: database initialization is added and configured with app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBCAuVLysfydu-aJGChyq0V1_x6vq5KF9Q",
+  authDomain: "chat-app-c62db.firebaseapp.com",
+  projectId: "chat-app-c62db",
+  storageBucket: "chat-app-c62db.appspot.com",
+  messagingSenderId: "1032966264712",
+  appId: "1:1032966264712:web:0b730f20e6ff948df84c3d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
